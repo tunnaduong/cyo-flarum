c29fff56c88b547223d2cc5e7b6f8d8a7b95ec8f
root <root@21052015022557duongtunganh.novalocal>
2025-06-27 13:16:42 +0700

    update

--- storage/views/69660e6636f815388aa2308a502f3e2b15189208.php
+++
@@ -1,2 +0,0 @@
-<img class="FoFUpload--Upl-Image-Preview" src="{@url}" title="{@title}" alt="{@alt}" data-id="{@uuid}" loading="lazy"/>
-<?php /**PATH C:\laragon\www\cyo-flarum\vendor\fof\upload/resources/templates/image-preview.blade.php ENDPATH**/ ?>
